88dee0c53f43c197f86ee8a6664705988ae1dff4
BreidysG <Breidysgutierrez8@gmail.com>
2024-10-17 18:43:15 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,186 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const titleInit = document.getElementById("titleInit");
+    const paragraphInit = document.getElementById("paragraphInit");
+    const startButton = document.getElementById('startButton');
+    const textContainer = document.getElementById('textContainer');
+    const finishButton = document.getElementById('finishButton');
+    const questionnaire = document.getElementById('questionnaire');
+    const quizForm = document.getElementById('quizForm');
+    const results = document.getElementById('results');
+    const questionOptions = document.querySelector('.question-options'); // Contenedor de las opciones de pregunta
+    const timerValue = document.getElementById('timerValue');
+    let time = [];
+
+    let wordCount = 0;
+    let startTime, endTime;
+    let quizCompleted = false;
+    let timerInterval;
+
+    // Texto para las preguntas del cuestionario
+    const questions = [
+        {
+            question: "¿Cuál es el título de la lectura?", 
+            answers: [
+                "Mamá y yo", 
+                "Caja de cartón", 
+                "Mi casa, una caja de cartón", 
+                "La caja de cartón y yo"
+            ], 
+            correctAnswer: "Caja de cartón"
+        },
+        {
+            question: "¿Cuál es la idea principal de la lectura?", 
+            answers: [
+                "La historia de una niña y su mamá inmigrantes que llegan a un nuevo país, y después de muchas situaciones, ella es adoptada por una familia que la quiere", 
+                "La historia de una familia pobre que vivía en una caja de cartón", 
+                "El relato de un náufrago y sus anécdotas", 
+                "Todas las respuestas son correctas"
+            ], 
+            correctAnswer: "La historia de una niña y su mamá inmigrantes que llegan a un nuevo país, y después de muchas situaciones, ella es adoptada por una familia que la quiere"
+        },
+        {
+            question: "¿Cuál fue la primera casa que conoció la niña?", 
+            answers: [
+                "El orfanato", 
+                "Una embarcación", 
+                "La caja de cartón", 
+                "Una casa"
+            ], 
+            correctAnswer: "La caja de cartón"
+        },
+        {
+            question: "¿Cuándo la embarcación cayó a pique a qué lugar llegaron la niña y su mamá?", 
+            answers: [
+                "A un puerto seguro", 
+                "Las recogió una embarcación más grande", 
+                "A una playa solitaria", 
+                "A un acuario"
+            ], 
+            correctAnswer: "A una playa solitaria"
+        },
+        {
+            question: "¿Qué pasó finalmente con la niña?", 
+            answers: [
+                "Fue adoptada por una familia que la quería mucho en un nuevo país", 
+                "Su mamá falleció y quedó sola en un orfanato", 
+                "La niña y su mamá fueron enviadas nuevamente a su país de origen", 
+                "Todas las respuestas son correctas"
+            ], 
+            correctAnswer: "Fue adoptada por una familia que la quería mucho en un nuevo país"
+        }
+    ];
+    
+    
+    
+              
+    // Función para contar las palabras en el texto
+    function countWords(text) {
+        return text.split(/\s+/).length;
+    }
+
+    // Función para calcular la velocidad de lectura
+    function calculateReadingSpeed(startTime, endTime, wordCount) {
+        const minutes = (endTime - startTime) / 60000; // Convertir a minutos
+        return Math.round(wordCount / minutes);
+    }
+
+    // Función para mostrar las preguntas del cuestionario
+    function displayQuestions() {
+        const questionList = document.getElementById('questionList');
+        questionList.innerHTML = '';
+        
+        questions.forEach((question, index) => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <p>${question.question}</p>
+                <div class="question-options">
+                    ${question.answers.map((answer, i) => `
+                        <label>
+                            <input type="radio" name="answer${index}" value="${answer}">
+                            ${answer}
+                        </label>
+                    `).join('')}
+                </div>
+            `;
+            questionList.appendChild(listItem);
+        });
+    }
+
+    // Evento para comenzar el test al hacer clic en "Empezar"
+    startButton.addEventListener('click', function() {
+        textContainer.classList.remove('hidden');
+        paragraphInit.classList.add("hidden");
+        startButton.classList.add('hidden');
+        startTime = Date.now();
+        // Iniciar el contador
+        timerInterval = setInterval(updateTimer, 1000);
+    });
+
+    // Función para actualizar el contador de tiempo
+    function updateTimer() {
+        const currentTime = Math.floor((Date.now() - startTime) / 1000); // Tiempo transcurrido en segundos
+        timerValue.textContent = currentTime;
+        time.push(currentTime);
+    }
+
+    // Evento para finalizar el test al hacer clic en "Terminar Test"
+    finishButton.addEventListener('click', function() {
+        clearInterval(timerInterval); // Detener el contador
+        titleInit.classList.add("hidden");
+        textContainer.classList.add('hidden');
+        finishButton.classList.add('hidden');
+        endTime = Date.now();
+        wordCount = countWords(document.getElementById('textToRead').textContent);
+        const readingSpeed = calculateReadingSpeed(startTime, endTime, wordCount);
+        questionnaire.classList.remove('hidden');
+        displayQuestions();
+    });
+
+    // Evento para enviar el cuestionario
+    quizForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        if (!quizCompleted) {
+            let anyAnswerSelected = true; // Inicialmente, asumimos que al menos una opción está seleccionada
+
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (!selectedAnswer) { // Si ninguna opción está seleccionada para alguna pregunta
+                    anyAnswerSelected = false;
+                    return;
+                }
+            });
+
+            if (!anyAnswerSelected) {
+                alert("Debes seleccionar una opción para cada pregunta antes de terminar.");
+                return;
+            }
+
+            let correctAnswers = 0;
+            let totalQuestions = questions.length;
+            
+            questions.forEach((question, index) => {
+                const selectedAnswer = document.querySelector(`input[name="answer${index}"]:checked`);
+                if (selectedAnswer && selectedAnswer.value === question.correctAnswer) {
+                    correctAnswers++;
+                }
+            });
+            
+            let comprehensionPercentage = (correctAnswers / totalQuestions) * 100;
+            let timeResult = time[time.length-1];
+            
+            quizCompleted = true;
+            results.classList.remove('hidden');
+            questionnaire.classList.add('hidden');
+            document.getElementById('wordCountValue').textContent = wordCount;
+            document.getElementById('readingSpeedValue').textContent = `${calculateReadingSpeed(startTime, endTime, wordCount)}`;
+            document.getElementById('comprehensionValue').textContent = `${comprehensionPercentage}`;
+            document.getElementById('timeResultValue').textContent = `${timeResult}`;
+            document.getElementById('results').innerHTML += '<p class="lastParagrah">Toma nota de tu velocidad de lectura para poder realizar ajustes en los próximos ejercicios.</p><p class="finalMessage">Puedes salir y pasar a la siguiente clase.</p>'
+            ;
+
+            
+            
+        }
+    });
+
+});
